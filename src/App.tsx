0a6e7b2ed9073f64ea3827147d51116ac867b0e4
Shanyce Benson <shanyce.benson@outlook.com>
2025-05-16 14:03:22 +0900

    prepare for page hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Interest from './pages/Interests';
-import Resume from './pages/Resume';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Game from './pages/Game';
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Interest from "./pages/Interests";
+import Resume from "./pages/Resume";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Game from "./pages/Game";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Layout>
-        <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/interests" element={<Interest />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/game" element={<Game />} />
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/interests" element={<Interest />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/game" element={<Game />} />
+          </Routes>
+        </Router>
       </Layout>
     </div>
   );
