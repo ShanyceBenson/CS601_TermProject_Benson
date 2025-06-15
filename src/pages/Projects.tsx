b52f0f4daf24c05b943b0e4c6ec5c643bab62cc4
Shanyce Benson <shanyce.benson@outlook.com>
2025-06-15 19:49:52 +0900

    updated the theme and finished projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,37 @@
+import projectsData from '../data/projectsData.json';
+import './Projects.css';
+
 function Projects() {
+/* const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("../data/projectsData.json")
+      .then((res) => res.json())
+      .then((data) => setProjects(data))
+      .catch((error) => console.error("Failed to load projects:", error));
+  }, []); */
+
   return (
-    <div>Projects</div>
+   <div className="projects-container">
+      <h1 className="projects-title">My CS601 Projects</h1>
+      <div className="projects-grid">
+        {projectsData.map((project, index) => (
+          <div className="project-card" key={index}>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+            <div className="project-links">
+              <a
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View on GitHub
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
